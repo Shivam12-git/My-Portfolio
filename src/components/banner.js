2b15f4ff8bb33f4b6a9d5726d5acb8bb4936ec89
Shivam <B20135@students.iitmandi.ac.in>
2023-09-19 13:47:31 +0530

    Updated my resume

--- src/components/banner.js
+++ src/components/banner.js
@@ -62,7 +62,7 @@ export const Banner = () => {
                                 <span id = "tagline">Bringing simplicity and elegance to complex problems is my approach to Product Design.</span>
                             </div>
                         </div>
-                        <a className="button" href="https://drive.google.com/file/d/1QABFvgSJHmTo4woRZonbj7Sg-qEb_-5V/view?usp=sharing" target="_blank" rel="noreferrer">
+                        <a className="button" href="https://drive.google.com/file/d/1JRjrSNFJpKo4BNmM3Rj-DcHIuVlJil8h/view?usp=sharing" target="_blank" rel="noreferrer">
                             <div className="button-layout">
                             <div className="dwnld-icon"><img src={dwnld} alt="Hdr" /></div>
                             <div id="resume-button">My Resume</div>
